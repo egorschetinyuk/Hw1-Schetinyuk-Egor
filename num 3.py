7d99f2cbd6c06fa2addde8fd53d9ff8f232f2ef6
egorschetinyuk <chepolette@yandex.ru>
2024-07-13 11:14:42 +0300

    Update num 3.py
    Изменен код для работы над ошибками

--- num 3.py
+++ num 3.py
@@ -1,21 +1,22 @@
-try:
-    with open('input1.txt', 'r') as perviy:
-        for lines in perviy:
-            itog1 = lines.strip()
-
-    with open('input2.txt', 'r') as vtoroiy:
-        for lines in vtoroiy:
-            itog2 = lines.strip()
-except ValueError:
-    print("некорректная строка")
-vmeste = itog1 + itog2
-sortirovochka = sorted(vmeste)
-print(sortirovochka)
-sortirovochka1=''
-for i in sortirovochka:
-    if i != '"' :
-        sortirovochka1 += i
-    else :
-        sortirovochka1 += " "
-with open('output2.txt', 'w') as file:
-    file.write(str((sortirovochka1)))
+while True:
+    try:
+        with open('input1.txt', 'r') as perviy:
+            for lines in perviy:
+                itog1 = lines.strip()
+        with open('input2.txt', 'r') as vtoroiy:
+            for lines in vtoroiy:
+                itog2 = lines.strip()
+        vmeste = itog1 + itog2
+        sortirovochka = sorted(vmeste)
+        print(sortirovochka)
+        sortirovochka1 = ''
+        for i in sortirovochka:
+            if i != '"':
+                sortirovochka1 += i
+            else:
+                sortirovochka1 += " "
+        with open('output2.txt', 'w') as file:
+            file.write(str((sortirovochka1)))
+            break
+    except FileNotFoundError:
+        print("Файл не найден")
