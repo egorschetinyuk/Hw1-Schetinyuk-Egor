5b1407fb5eabb84f007163e5283904bea82a152f
egorschetinyuk <chepolette@yandex.ru>
2024-07-13 11:24:16 +0300

    Update num 1.py
    Изменения для работы над ошибками

--- num 1.py
+++ num 1.py
@@ -1,20 +1,19 @@
-file= open('output.txt', 'w')
-names = []
-grades = []
-try:
-    ocenki = open('input.txt', 'r')
-    for line in ocenki:
-        name, grade = line.strip().split(',')
-        grade = int(grade)
-        grades.append(grade)
-        names.append(name)
-        print(grades)
-        print(names)
-        srednya = sum(grades) / len(grades)
-        print(srednya)
-        for i in range(len(grades)):
-            if grades[i] > srednya:
-                file.write(f"{names[i]},{grades[i]}\n")
-    file.close()
-except ValueError:
-    print("файл не найден")
+while True:
+    try:
+        with open('output.txt', 'a') as file:
+            names = []
+            grades = []
+            with open('input.txt', 'r') as ocenki:
+                for line in ocenki:
+                    name, grade = line.strip().split(',')
+                    grade = int(grade)
+                    grades.append(grade)
+                    names.append(name)
+            srednya = sum(grades) / len(grades)
+            print(srednya)
+            for i in range(len(grades)):
+                if grades[i] > srednya:
+                    file.write(f"{names[i]},{grades[i]}\n")
+        break
+    except FileNotFoundError:
+        print("Файл не найден")
